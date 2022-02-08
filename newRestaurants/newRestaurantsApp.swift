9b829645b404c7123df7233858110b86d58c342d
Haley Parker <buddza12@gmail.com>
2022-02-08 15:51:14 -0600

    Initial Commit

---
+++ newRestaurants/newRestaurantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  newRestaurantsApp.swift
+//  newRestaurants
+//
+//  Created by 90306479 on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct newRestaurantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
